93f6f77193c99e50a03b1431848eec71205a614d
lsotoqu <luissotopratulla@gmail.com>
2017-02-25 13:50:44 -0500

    Se realizo insumo

--- routes/web.php
+++ routes/web.php
@@ -24,32 +24,14 @@ Route::get('/usuario/registrar', 'usuarioController@create');
 Route::post('/usuario/crear', 'usuarioController@save');
 Route::get('servicio/get', 'servicioController@getData');
 
+
 Route::resource('servicio','servicioController');
 Route::resource('contrato','tipoContratoController');
 Route::resource('clinica','clinicaController');
 Route::resource('usuario','usuarioController');
-<<<<<<< HEAD
 Route::resource('insumo','insumoController');
-=======
 Route::resource('servicioTipoContrato','servicioTipoContratoController');
->>>>>>> f97e1124555a398bf6ddca3d477f06e2740b472d
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index');
-
-<<<<<<< HEAD
 Route::resource('/proveedor', 'proveedorController');
-
 Route::resource('/ordendeproduccion', 'ordendeproduccionController');
-=======
 
->>>>>>> e1bac586f63620a05412c8fe6f3f87d7a474ebf3
+
